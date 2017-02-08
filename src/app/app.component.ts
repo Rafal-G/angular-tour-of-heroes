7c23baf0f14e7630a037c3ebaf87305819834e2a
Rafal Gostynski <rgostynski@gmail.com>
2017-02-08 13:39:16 +0200

    Service and getting data via promise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import {Component} from '@angular/core';
 import {Hero} from './models/hero';
+import {HeroService} from './services/hero.service';
+import {OnInit} from '@angular/core';
+
 
 @Component({
+  providers: [HeroService],
   selector: 'my-app',
   styles: [`
       .selected {
@@ -64,24 +68,23 @@ import {Hero} from './models/hero';
               <my-hero-detail [hero]="selectedHero"></my-hero-detail>`
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private heroService: HeroService) { }
+  heroes: Hero[];
+  selectedHero: Hero;
+
   onSelect(hero :Hero)  {
     this.selectedHero = hero;
   };
-  heroes = HEROES;
-  selectedHero: Hero;
+
   title = 'Tour of Heroes';
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes)
+
+  }
 }
 
-const HEROES: Hero[] = [
-  {id: 11, name: 'Mr. Nice'},
-  {id: 12, name: 'Narco'},
-  {id: 13, name: 'Bombasto'},
-  {id: 14, name: 'Celeritas'},
-  {id: 15, name: 'Magneta'},
-  {id: 16, name: 'RubberMan'},
-  {id: 17, name: 'Dynama'},
-  {id: 18, name: 'Dr IQ'},
-  {id: 19, name: 'Magma'},
-  {id: 20, name: 'Tornado'}
-];
