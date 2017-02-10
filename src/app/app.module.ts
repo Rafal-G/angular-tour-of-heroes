99a05cd6d069c7db8f5bccab535f7e393f486b50
Rafal Gostynski <rgostynski@gmail.com>
2017-02-10 20:22:54 +0200

    Delete and add heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,35 @@
-import {NgModule}      from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule}   from '@angular/forms';
-import {HeroDetailComponent} from './hero-detail.component';
-import {HeroComponent} from './hero.component';
-import {AppComponent}  from './app.component';
-import {HeroService} from './services/hero.service';
-import {DashboardComponent} from "./dashboard.component";
-import {AppRoutingModule} from "./app-routing.module";
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
+import { AppRoutingModule } from './app-routing.module';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './services/in-memory-data.service';
+
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroComponent }      from './hero.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroService }          from './services/hero.service';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroComponent,
-    DashboardComponent],
-  bootstrap: [AppComponent],
-  providers: [HeroService]
+  ],
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule {
-}
+export class AppModule { }
--- src/app/hero.component.ts
+++ src/app/hero.component.ts
@@ -35,5 +35,24 @@ export class HeroComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes)
   }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+  delete(hero: Hero): void {
+    this.heroService
+      .delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      });
+  }
+
 }
 
